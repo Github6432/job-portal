f7543c99deacf30377742d2a698388ab8c1b1d54
Github6432 <parasshankhwar2@gmail.com>
2024-11-01 08:04:21 +0530

    fix layout & Add Navbar in layout page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <div>About</div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam beatae sunt eius, at enim mollitia unde possimus odit! Laboriosam dolores voluptatem, possimus beatae quo asperiores recusandae provident labore! Facere nesciunt fuga labore aliquam dignissimos facilis ex culpa numquam, praesentium nostrum sunt excepturi, hic illum eum, consectetur nobis itaque. Aliquid, dignissimos a? Nostrum cumque doloremque, facilis vero ipsum, minus, facere dolor ab quo ratione quos ea ipsa. Ipsam blanditiis ratione error ipsum dolorem obcaecati, eligendi veritatis, reprehenderit explicabo possimus exercitationem delectus illo ex eveniet labore modi provident incidunt voluptatem quisquam fuga expedita accusamus deleniti. Ducimus sed reprehenderit quae! Cupiditate, voluptas quidem.</p>
+    </div>
+  )
+}
+
+export default About
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="bg-gray-800 text-white px-4 py-3 shadow-md fixed top-0 left-0 w-full z-50">
+    <nav className="bg-gray-800  text-white px-4 py-3 shadow-md fixed top-0 left-0 w-full z-50">
       <div className="container mx-auto px-6 flex items-center justify-between">
         {/* Logo */}
         <Link href="/" legacyBehavior>
